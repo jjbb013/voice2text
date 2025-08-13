9f9280798fd9f696f0caad2c1cb2665ce3e2ba41
Will <will_pan@me.com>
2025-08-13 16:27:30 +0800

    Update README.md with Deploy button

--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,33 @@ function isFile(value: any): value is File {
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    if (request.method !== 'POST') {
-      return new Response('Expected POST request', { status: 405 });
-    }
-
-    const formData = await request.formData();
-    const audioFile = formData.get('audio');
-
-    if (!isFile(audioFile)) {
-      return new Response('No audio file uploaded or the uploaded item is not a file.', { status: 400 });
-    }
-
-    const audioArrayBuffer = await audioFile.arrayBuffer();
-
-    const response = await env.AI.run(
-      '@cf/openai/whisper',
-      {
-        audio: [...new Uint8Array(audioArrayBuffer)],
+    try {
+      if (request.method !== 'POST') {
+        return new Response('Expected POST request', { status: 405 });
       }
-    );
 
-    return new Response(JSON.stringify(response), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+      const formData = await request.formData();
+      const audioFile = formData.get('audio');
+
+      if (!isFile(audioFile)) {
+        return new Response('No audio file uploaded or the uploaded item is not a file.', { status: 400 });
+      }
+
+      const audioArrayBuffer = await audioFile.arrayBuffer();
+
+      const response = await env.AI.run(
+        '@cf/openai/whisper',
+        {
+          audio: [...new Uint8Array(audioArrayBuffer)],
+        }
+      );
+
+      return new Response(JSON.stringify(response), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } catch (e: any) {
+      return new Response(e.message, { status: 500 });
+    }
   },
 };
 
